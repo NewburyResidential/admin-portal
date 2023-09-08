a6e9beaab9c30c29cd89031ad6d6e529796ca425
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-08 12:24:30 -0400

    Batch API To Run Concurrently

--- src/app/api/documentAI/route.js
+++ src/app/api/documentAI/route.js
@@ -1,103 +1,78 @@
 import { NextResponse } from 'next/server';
 const { DocumentProcessorServiceClient } = require('@google-cloud/documentai').v1beta3;
 const { Storage } = require('@google-cloud/storage');
-import PQueue from 'p-queue';
 
 const client = new DocumentProcessorServiceClient();
 const storage = new Storage();
 
-const bucketName = 'pdf-bucket-storage-1'; // Replace with your GCS bucket name
+const currentDateTime = new Date();
+const batchId = currentDateTime.toISOString().replace(/Z$/, '_EST');
+
+const bucketName = 'pdf-bucket-storage-1';
 const projectId = '609778734661';
 const location = 'us';
 const processorId = '9bbdea563b5e2c4a';
 const gcsOutputUri = 'gs://pdf-bucket-storage-1';
-const gcsOutputUriPrefix = 'output/results'; // Replace with your desired prefix
-
+const gcsOutputUriPrefix = `${batchId}/output`;
 const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
 
 export async function POST(request) {
-  let docRes = []
   try {
     const formData = await request.formData();
-    const processingPromises = [];
-    const documentResponses = []; // Store document responses here
+    const documentResponses = []; // Store document responses herel
+    const uploadedDocuments = []; // Store uploaded document objects here
 
     for (const [fieldName, file] of formData.entries()) {
       const fileName = file.name;
       const fileBuffer = Buffer.from(await file.arrayBuffer());
 
       // Upload the file to GCS
-      const uploadPromise = storage.bucket(bucketName).file(fileName).save(fileBuffer);
+      await storage.bucket(bucketName).file(`${batchId}/input/${fileName}`).save(fileBuffer);
       console.log(`Uploaded ${fileName} to GCS bucket.`);
 
-      const gcsInputUri = `gs://${bucketName}/${fileName}`;
-
-      const docRequest = {
-        name,
-        inputDocuments: {
-          gcsDocuments: {
-            documents: [
-              {
-                gcsUri: gcsInputUri,
-                mimeType: 'application/pdf',
-              },
-            ],
-          },
-        },
-        outputConfig: {
-          gcsDestination: `${gcsOutputUri}/${gcsOutputUriPrefix}/`,
-        },
-      };
-
-      // Trigger Document AI processing for the current document without waiting
-      const processingPromise = client.batchProcessDocuments(docRequest);
-
-      // Add a status update promise to log when processing is complete
-      const statusUpdatePromise = processingPromise.then(([operation]) => {
-        console.log(`Started Document AI processing for ${fileName}`);
-        return operation.promise().then((response) => {
-          console.log(`Document AI processing complete for ${fileName}`);
-          // Store the response in the array
-          documentResponses.push({ fileName, response });
-        });
-      });
-
-      processingPromises.push(statusUpdatePromise);
-
-      // Continue uploading and processing the next document while this one is processing
-      await uploadPromise;
+        // Create a document object for the uploaded file
+        const gcsUri = `gs://${bucketName}/${batchId}/input/${fileName}`;
+        const mimeType = file.type; //Review for png etc
+        uploadedDocuments.push({ gcsUri, mimeType });
     }
 
-    // Wait for all processing operations and status updates to complete
-    await Promise.all(processingPromises);
-    console.log('All Document AI processing completed.');
-  
+    const docRequest = {
+      name,
+      inputDocuments: {
+        gcsDocuments: {
+          documents: uploadedDocuments,
+        },
+      },
+      outputConfig: {
+        gcsDestination: `${gcsOutputUri}/${gcsOutputUriPrefix}/`,
+      },
+    };
+
+    // Trigger Document AI processing for the current document
+    const [operation] = await client.batchProcessDocuments(docRequest);
+    console.log(`Started batch processing`);
+    const [response] = await operation.promise();
+    console.log(`Document batch processing complete`);
+
     const query = {
       prefix: gcsOutputUriPrefix,
     };
-  
+
     console.log('Fetching results ...');
-  
+
     // List all of the files in the Storage bucket
     const [files] = await storage.bucket(gcsOutputUri).getFiles(query);
-  
-    // Add all asynchronous downloads to queue for execution.
-    const queue = new PQueue({concurrency: 15});
-    const tasks = files.map((fileInfo) => async () => {
+
+    // Process the downloaded files (you can add more logic here)
+    for (const fileInfo of files) {
       const [file] = await fileInfo.download();
       const document = JSON.parse(file.toString());
-      docRes.push(document.entities)
-    });
-    await queue.addAll(tasks);
-
-
-
-
-
+      documentResponses.push(document)
+    }
 
     // Return the document responses in the JSON response to the client
     return new Response(
-      JSON.stringify({ message: 'Files uploaded and processed successfully', docRes }),
+      JSON.stringify({ message: 'Files uploaded and processed successfully', documentResponses }),
       {
         headers: { 'Content-Type': 'application/json' },
         status: 200,
