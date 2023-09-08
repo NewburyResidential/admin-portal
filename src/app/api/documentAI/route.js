56cffd41c888e8e3ce6e9ccaeaaf0bc41d6c9b4e
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-08 13:15:52 -0400

    Slight Refactor

--- src/app/api/documentAI/route.js
+++ src/app/api/documentAI/route.js
@@ -5,18 +5,18 @@ const { Storage } = require('@google-cloud/storage');
 const client = new DocumentProcessorServiceClient();
 const storage = new Storage();
 
-const currentDateTime = new Date();
-const batchId = currentDateTime.toISOString().replace(/Z$/, '_EST');
-
-const bucketName = 'pdf-bucket-storage-1';
-const projectId = '609778734661';
-const location = 'us';
-const processorId = '9bbdea563b5e2c4a';
-const gcsOutputUri = 'gs://pdf-bucket-storage-1';
-const gcsOutputUriPrefix = `${batchId}/output`;
-const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
-
 export async function POST(request) {
+  const currentDateTime = new Date();
+  const batchId = currentDateTime.toISOString().replace(/Z$/, '_EST');
+
+  const bucketName = 'pdf-bucket-storage-1';
+  const projectId = '609778734661';
+  const location = 'us';
+  const processorId = '9bbdea563b5e2c4a';
+  const gcsOutputUri = 'gs://pdf-bucket-storage-1';
+  const gcsOutputUriPrefix = `${batchId}/output`;
+  const name = `projects/${projectId}/locations/${location}/processors/${processorId}`;
+
   try {
     const formData = await request.formData();
     const documentResponses = []; // Store document responses herel
@@ -25,15 +25,14 @@ export async function POST(request) {
     for (const [fieldName, file] of formData.entries()) {
       const fileName = file.name;
       const fileBuffer = Buffer.from(await file.arrayBuffer());
-
       // Upload the file to GCS
       await storage.bucket(bucketName).file(`${batchId}/input/${fileName}`).save(fileBuffer);
       console.log(`Uploaded ${fileName} to GCS bucket.`);
 
-        // Create a document object for the uploaded file
-        const gcsUri = `gs://${bucketName}/${batchId}/input/${fileName}`;
-        const mimeType = file.type; //Review for png etc
-        uploadedDocuments.push({ gcsUri, mimeType });
+      // Create a document object for the uploaded file
+      const gcsUri = `gs://${bucketName}/${batchId}/input/${fileName}`;
+      const mimeType = file.type; //Review for png etc
+      uploadedDocuments.push({ gcsUri, mimeType });
     }
 
     const docRequest = {
@@ -67,7 +66,7 @@ export async function POST(request) {
     for (const fileInfo of files) {
       const [file] = await fileInfo.download();
       const document = JSON.parse(file.toString());
-      documentResponses.push(document)
+      documentResponses.push(document);
     }
 
     // Return the document responses in the JSON response to the client
