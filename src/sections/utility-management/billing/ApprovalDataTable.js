146860f037554ed8346e80d96ebd1fcd1ad3bbd1
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-14 23:26:02 -0400

    Working On New Table

--- src/sections/utility-management/billing/ApprovalDataTable.js
+++ src/sections/utility-management/billing/ApprovalDataTable.js
@@ -1,97 +1,242 @@
+import * as React from 'react';
 import PropTypes from 'prop-types';
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import { DataGrid } from '@mui/x-data-grid';
-import Iconify from 'src/components/iconify';
-import { _mock } from 'src/_mock';
-import { Box, Tooltip, Typography } from '@mui/material';
-import Link from 'next/link';
+import Tooltip from '@mui/material/Tooltip';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import DeleteIcon from '@mui/icons-material/Delete';
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
 
-const cellStyles = (params) => {
-  return;
-};
-
-// Define a custom cell renderer for the firstName column
-const billLinkColumn = {
-  field: 'billImageLink',
-  headerName: 'Image',
-  align: 'center',
-  headerAlign: 'center',
-  sortable: false,
-  disableColumnMenu: true,
-  renderCell: (params) => (
-    <Link href={`${params.value}`} target="_blank">
-      <IconButton>
-        <Iconify icon="solar:bill-list-linear" />
-      </IconButton>
-    </Link>
-  ),
-};
-
-const editableColumn = (headerName, value) => {
+function createData(name, calories, fat, carbs, protein) {
   return {
-    field: value,
-    headerName,
-    align: 'center',
-    headerAlign: 'center',
-    editable: true,
-    disableColumnMenu: true,
-    flex: 1,
-    renderCell: (params) => {
-      const confidence = params.row.confidence[value];
-      return (
-        <Box
-          sx={{
-            backgroundColor: confidence < 1 ? '#FFCCCB' : 'initial',
-            width: '100%',
-            height: '100%',
-            paddingLeft: '10px',
-            paddingRight: '10px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Typography>{params.value}</Typography>
-          {confidence < 1 && (
-            <Typography variant="caption" fontSize={'11px'} color={'#637381'}>
-              {Number(confidence).toFixed(2).replace(/^0/, '')}
-            </Typography>
-          )}
-        </Box>
-      );
-    },
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
   };
-};
+}
 
-const columns = [
-  editableColumn('Utility', 'utilityType'),
-  editableColumn('Meter Number', 'meterNumber'),
-  editableColumn('Bill Month', 'billMonth'),
-  editableColumn('Start Service', 'startReadDate'),
-  editableColumn('End Service', 'endReadDate'),
-  editableColumn('Amount', 'amount'),
-  billLinkColumn,
+const rows = [
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Donut', 452, 25.0, 51, 4.9),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Honeycomb', 408, 3.2, 87, 6.5),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Jelly Bean', 375, 0.0, 94, 0.0),
+  createData('KitKat', 518, 26.0, 65, 7.0),
+  createData('Lollipop', 392, 0.2, 98, 0.0),
+  createData('Marshmallow', 318, 0, 81, 2.0),
+  createData('Nougat', 360, 19.0, 9, 37.0),
+  createData('Oreo', 437, 18.0, 63, 4.0),
 ];
 
-export default function ApprovalDataTable({ data }) {
+function stableSort(array) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+ console.log(stabilizedThis)
+  return stabilizedThis.map((el) => el[0]);
+}
+
+// const headCells = [
+//   { id: 'utilityType', label: 'Utility Type' },
+//   { id: 'meterNumber', label: 'Meter Number' },
+//   { id: 'serviceStart', label: 'Service Start' },
+//   { id: 'serviceEnd', label: 'Service End' },
+//   { id: 'amount', label: 'Amount' },
+//   { id: 'tax', label: 'Tax' },
+//   { id: 'totalAmount', label: 'Total Amount' },
+// ];
+
+
+function EnhancedTableToolbar(props) {
+  const { numSelected, rowCount, onSelectAllClick } = props;
+
   return (
-    <DataGrid
+    <Toolbar
       sx={{
-        '& .MuiDataGrid-cell': {
-          p: 0,
-        },
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+        }),
       }}
-      columns={columns}
-      rows={data}
-      checkboxSelection
-      disableRowSelectionOnClick
-      density={'comfortable'}
-      pageSizeOptions={[]}
-    />
+    >
+      <Checkbox
+        color="primary"
+        indeterminate={numSelected > 0 && numSelected < rowCount}
+        checked={rowCount > 0 && numSelected === rowCount}
+        onChange={onSelectAllClick}
+        inputProps={{
+          'aria-label': 'select all desserts',
+        }}
+        sx={{ marginRight: 1 }}
+      />
+      {numSelected > 0 ? (
+        <Typography sx={{ flex: '1 1 100%' }} color="inherit" variant="subtitle1" component="div">
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography sx={{ flex: '1 1 100%' }} variant="h6" id="tableTitle" component="div">
+          Approval
+        </Typography>
+      )}
+
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : null}
+    </Toolbar>
   );
 }
 
-ApprovalDataTable.propTypes = {
-  data: PropTypes.array,
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
 };
+
+export default function EnhancedTable() {
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.name);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const visibleRows = React.useMemo(
+    () =>
+      stableSort(rows).slice(
+        page * rowsPerPage,
+        page * rowsPerPage + rowsPerPage
+      ),
+    [page, rowsPerPage]
+  );
+
+  console.log(visibleRows)
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Paper sx={{ width: '100%', mb: 2 }}>
+        <EnhancedTableToolbar
+          numSelected={selected.length}
+          onSelectAllClick={handleSelectAllClick}
+          rowCount={rows.length}
+        />
+          {/* <EnhancedTableHead
+              numSelected={selected.length}
+              onSelectAllClick={handleSelectAllClick}
+              rowCount={rows.length}
+            /> */}
+        <TableContainer sx={{ maxHeight: 600, overflow: 'auto' }}>
+          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
+          
+            <TableBody>
+              {visibleRows.map((row, index) => {
+                const isItemSelected = isSelected(row.name);
+                const labelId = `enhanced-table-checkbox-${index}`;
+
+                return (
+                  <>
+                    <TableRow key={row.name} selected={isItemSelected}>
+                      <TableCell padding="checkbox" style={{ padding: '16px 8px', }}>
+                        <Checkbox
+                          color="primary"
+                          onClick={(event) => handleClick(event, row.name)}
+                          checked={isItemSelected}
+                          inputProps={{
+                            'aria-labelledby': labelId,
+                          }}
+                        />
+                      </TableCell>
+                      <TableCell id={labelId} scope="row" >
+                        1342342333
+                      </TableCell>
+                 
+                    </TableRow>
+                    <TableRow key={row.name} selected={isItemSelected} >
+                      <TableCell colSpan={10} style={{ padding: '6px 6px' }}>
+                        <Table size="small" aria-label="utility">
+                          <TableBody>
+                            <TableRow key={'4'}>
+                              <TableCell align="center">Utility type</TableCell>
+                              <TableCell align="center">Methjgkhjghgjghhber Number</TableCell>
+                              <TableCell align="center">Service Start</TableCell>
+                              <TableCell align="center">Service End</TableCell>
+                              <TableCell align="center">Amount</TableCell>
+                              <TableCell align="center">Tax</TableCell>
+                              <TableCell align="center">Total Amount</TableCell>
+                            </TableRow>
+                          </TableBody>
+                        </Table>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+        />
+      </Paper>
+    </Box>
+  );
+}
