7febed7d7dedaaf5046e024845cb2e4f6ff12202
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-15 00:01:33 -0400

    Progress On Table

--- src/sections/utility-management/billing/ApprovalDataTable.js
+++ src/sections/utility-management/billing/ApprovalDataTable.js
@@ -22,35 +22,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import { visuallyHidden } from '@mui/utils';
 
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
-
 function stableSort(array) {
   const stabilizedThis = array.map((el, index) => [el, index]);
- console.log(stabilizedThis)
+  console.log(stabilizedThis);
   return stabilizedThis.map((el) => el[0]);
 }
 
@@ -63,7 +37,6 @@ function stableSort(array) {
 //   { id: 'tax', label: 'Tax' },
 //   { id: 'totalAmount', label: 'Total Amount' },
 // ];
-
 
 function EnhancedTableToolbar(props) {
   const { numSelected, rowCount, onSelectAllClick } = props;
@@ -114,14 +87,14 @@ EnhancedTableToolbar.propTypes = {
   numSelected: PropTypes.number.isRequired,
 };
 
-export default function EnhancedTable() {
+export default function EnhancedTable({ rows }) {
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
+      const newSelected = rows.map((r) => r.id);
       setSelected(newSelected);
       return;
     }
@@ -156,15 +129,11 @@ export default function EnhancedTable() {
   const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
   const visibleRows = React.useMemo(
-    () =>
-      stableSort(rows).slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage
-      ),
+    () => stableSort(rows).slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
     [page, rowsPerPage]
   );
 
-  console.log(visibleRows)
+  console.log(visibleRows);
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -174,51 +143,52 @@ export default function EnhancedTable() {
           onSelectAllClick={handleSelectAllClick}
           rowCount={rows.length}
         />
-          {/* <EnhancedTableHead
+        {/* <EnhancedTableHead
               numSelected={selected.length}
               onSelectAllClick={handleSelectAllClick}
               rowCount={rows.length}
             /> */}
         <TableContainer sx={{ maxHeight: 600, overflow: 'auto' }}>
           <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-          
             <TableBody>
-              {visibleRows.map((row, index) => {
-                const isItemSelected = isSelected(row.name);
+              {visibleRows.map((invoice, index) => {
+                const isItemSelected = isSelected(invoice.id);
                 const labelId = `enhanced-table-checkbox-${index}`;
 
                 return (
                   <>
-                    <TableRow key={row.name} selected={isItemSelected}>
-                      <TableCell padding="checkbox" style={{ padding: '16px 8px', }}>
+                    <TableRow key={invoice.id} selected={isItemSelected}>
+                      <TableCell padding="checkbox" style={{ padding: '16px 8px' }}>
                         <Checkbox
                           color="primary"
-                          onClick={(event) => handleClick(event, row.name)}
+                          onClick={(event) => handleClick(event, invoice.id)}
                           checked={isItemSelected}
                           inputProps={{
                             'aria-labelledby': labelId,
                           }}
                         />
                       </TableCell>
-                      <TableCell id={labelId} scope="row" >
-                        1342342333
+                      <TableCell id={labelId} scope="row">
+                        {invoice.id}
                       </TableCell>
-                 
                     </TableRow>
-                    <TableRow key={row.name} selected={isItemSelected} >
-                      <TableCell colSpan={10} style={{ padding: '6px 6px' }}>
+
+                    <TableRow selected={isItemSelected}>
+                      <TableCell colSpan={10} style={{ padding: '4px 6px' }}>
                         <Table size="small" aria-label="utility">
-                          <TableBody>
-                            <TableRow key={'4'}>
-                              <TableCell align="center">Utility type</TableCell>
-                              <TableCell align="center">Methjgkhjghgjghhber Number</TableCell>
-                              <TableCell align="center">Service Start</TableCell>
-                              <TableCell align="center">Service End</TableCell>
-                              <TableCell align="center">Amount</TableCell>
-                              <TableCell align="center">Tax</TableCell>
-                              <TableCell align="center">Total Amount</TableCell>
-                            </TableRow>
-                          </TableBody>
+                          {invoice.bills.map((bill) => (
+                            <TableBody>
+                              <TableRow key={'4'}>
+                                <TableCell align="center">{bill.utilityType}</TableCell>
+                                <TableCell align="center">{bill.meterNumber}</TableCell>
+                                <TableCell align="center">{bill.serviceStart}</TableCell>
+                                <TableCell align="center">{bill.serviceEnd}</TableCell>
+                                <TableCell align="center">{bill.amount}</TableCell>
+                                <TableCell align="center">{bill.tax}</TableCell>
+                                <TableCell align="center">{bill.totalAmount}</TableCell>
+                              </TableRow>
+                            </TableBody>
+                          ))}
                         </Table>
                       </TableCell>
                     </TableRow>
