c18f085089e2e4511d2e6be8ea0e1d27a3ec3c69
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-16 17:59:50 -0400

    Included Protected Middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,22 @@
-export {default} from "next-auth/middleware"
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
 
-export const config = { matcher: ["/dashboard/:path*", "/api/auth/:path*"]}
+export async function middleware(req) {
+  if (req.nextUrl.pathname === '/api/auth/') {
+    return NextResponse.next(); 
+  }
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  const { data: { session } } = await supabase.auth.getSession();
+  if (session?.user?.email) {
+    return res
+  }
+  const redirectUrl = req.nextUrl.clone()
+  redirectUrl.pathname = '/auth/login'
+  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname)
+  return NextResponse.redirect(redirectUrl)
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/api/:path*'] // PROTECTED_ROUTES
+}
