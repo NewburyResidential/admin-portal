c8f50fd116b3e5f04070c04eb64798842f7de747
Mikeaxio <mikeaxiotakis@gmail.com>
2023-09-16 21:18:09 -0400

    Login Adjustment

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,33 +1,32 @@
 'use client';
 
 import PropTypes from 'prop-types';
-import { useRouter } from 'next/router'; 
+import { useRouter } from 'next/navigation';
 
 import { useEffect } from 'react';
 import { SplashScreen } from 'src/components/loading-screen';
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
 export default function Login({ redirectPath }) {
-  const router = useRouter(); 
+  const router = useRouter();
   const supabase = createClientComponentClient();
-  const signInWithAzure = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: 'azure',
-      options: {
-        scopes: 'email',
-        redirectTo: `${router.basePath}/api/auth/?redirectedFrom=${redirectPath}`,
-      },
-    });
-  };
 
-  // useEffect(() => {
-  //   signInWithAzure();
-  // }, [signInWithAzure]);
-  signInWithAzure();
-  return <SplashScreen />
+  useEffect(() => {
+    const signInWithAzure = async () => {
+      await supabase.auth.signInWithOAuth({
+        provider: 'azure',
+        options: {
+          scopes: 'email',
+          redirectTo: `${router.basePath}/api/auth/?redirectedFrom=${redirectPath}`,
+        },
+      });
+    };
+    signInWithAzure();
+  }, [router.basePath, redirectPath, supabase.auth]);
 
+  return <SplashScreen />;
 }
 
 Login.propTypes = {
-  redirectPath: PropTypes.string
+  redirectPath: PropTypes.string,
 };
--- src/middleware.js
+++ src/middleware.js
@@ -8,7 +8,6 @@ export async function middleware(req) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
   const { data: { session } } = await supabase.auth.getSession();
-  console.log(session)
   if (session?.user?.email) {
     return res
   }
