c3d5343478598742bb88446cc51c60df71aa4031
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-12-01 01:23:25 -0500

    Transaction API

---
+++ src/utils/services/CCExpenses/getUnapprovedTransactions.js
@@ -0,0 +1,20 @@
+export default async function getUnapprovedTransactions() {
+  const url = 'https://0yxexcpp8f.execute-api.us-east-1.amazonaws.com/unapprovedTransactions';
+  const requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  try {
+    const response = await fetch(url, requestOptions);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data: ', error);
+  }
+}
