36ab891b8025ce983d3c4d45455d121826abca55
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-12-30 15:09:40 -0500

    Dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,25 +1,12 @@
-
-import { Container, Typography } from '@mui/material';
+import { getServerSession } from 'next-auth';
+import View from './components/View';
 
 export const metadata = {
   title: 'Dashboard',
 };
 
 export default async function Page() {
-  return (
-    <Container >
-    <Typography variant="h4">Welcome To Newbury&apos;s Admin Portal!</Typography>
+  const session = await getServerSession();
 
-    {/* <Box
-      sx={{
-        mt: 5,
-        width: 1,
-        height: 320,
-        borderRadius: 2,
-        bgcolor: (theme) => alpha(theme.palette.grey[500], 0.04),
-        border: (theme) => `dashed 1px ${theme.palette.divider}`,
-      }}
-    /> */}
-  </Container>
-  );
+  return <View user={session?.user} />;
 }
