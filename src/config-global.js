47a627d5cf16e407050924704c6445290d0a3984
Michael Axiotakis <maxiotakis@newburyresidential.com>
2023-12-30 11:00:47 -0500

    DELETE UTILITY COMPONENTS

--- src/config-global.js
+++ src/config-global.js
@@ -2,19 +2,6 @@ const mode = 'dev'; // dev | prod
 
 // API ----------------------------------------------------------------------
 
-export const SUPABASE_API = {
-  url: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  key: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-};
-
-export const AZURE_FORM_RECOGNIZER = {
-formEndpoint: "https://document-intelligence-utilities.cognitiveservices.azure.com/",
-formApiKey: process.env.AZURE_FORM_RECOGNIZER_API_KEY,
-storageContainer: "consumers-energy-gas-electric",
-storageConnectionString: process.env.AZURE_FORM_RECOGNIZER_STORAGE_CONNECTION_STRING,
-storageName: process.env.AZURE_FORM_RECOGNIZER_STORAGE_NAME,
-storageKey: process.env.AZURE_FORM_RECOGNIZER_STORAGE_KEY,
-}
 
 export const ENTRATA_API = {
 baseUrl: mode === 'production' ? process.env.ENTRATA_BASEURL : process.env.ENTRATA_DEV_BASEURL,
