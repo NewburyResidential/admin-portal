9dbe455dba3b2317cfa1c092a8445f778486a2bd
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-04 13:03:25 -0500

    Update Vendor DropDown

--- src/app/dashboard/credit-cards/components/DropDownVendor.js
+++ src/app/dashboard/credit-cards/components/DropDownVendor.js
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 
 import Box from '@mui/material/Box';
 import Popper from '@mui/material/Popper';
@@ -7,31 +7,45 @@ import FormControl from '@mui/material/FormControl';
 import Autocomplete from '@mui/material/Autocomplete';
 
 import { isMissingValue } from 'src/utils/expense-calculations/missing-value';
+import Iconify from 'src/components/iconify';
 
-export default function DropDownVendor({ vendors, handleVendorChange, item, isVendorRequired }) {
+export default function DropDownVendor({ handleOpen, vendors, handleVendorChange, item, isVendorRequired }) {
   const currentValue = item.vendor ? item.vendor : null;
   const merchantName = item.merchant ? item.merchant : '*Not Provided*';
   const [inputValue, setInputValue] = useState('');
 
   const optionsLimit = 7;
 
-  const filteredOptions = useMemo(() => {
-    return vendors.filter((vendor) => vendor.name.toLowerCase().includes(inputValue.toLowerCase())).slice(0, optionsLimit);
-  }, [inputValue, vendors]);
+  const filterOptions = (options, { inputValue }) => {
+    const filtered = options.filter((option) => option.name.toLowerCase().includes(inputValue.toLowerCase()));
+
+    const limitedFiltered = filtered.slice(0, optionsLimit - 1);
+    return [...limitedFiltered, { name: 'Add Vendor', id: 'add-vendor' }];
+  };
 
   return isVendorRequired ? (
     <FormControl fullWidth>
       <Autocomplete
+        filterOptions={filterOptions}
         PopperComponent={({ style, ...props }) => <Popper {...props} sx={{ ...style, height: 0 }} />}
         id="vendor-autocomplete"
-        options={filteredOptions}
+        options={vendors}
         getOptionLabel={(option) => option.name}
         value={currentValue}
+        isOptionEqualToValue={(option, selected) => option.id === selected.id}
         onChange={(event, newValue) => {
-          handleVendorChange(item.id, newValue);
+          if (newValue && newValue.id === 'add-vendor') {
+            handleOpen(inputValue);
+          } else {
+            handleVendorChange(item.id, newValue);
+          }
         }}
         onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
+          if (newInputValue.id !== 'add-vendor') {
+            setInputValue(newInputValue);
+          } else {
+            setInputValue(null);
+          }
         }}
         renderInput={(params) => {
           const { ...rest } = params;
@@ -45,6 +59,22 @@ export default function DropDownVendor({ vendors, handleVendorChange, item, isVe
           );
         }}
         renderOption={(props, option) => {
+          if (option.id === 'add-vendor') {
+            return (
+              <li
+                {...props}
+                key={option.id}
+                style={{
+                  fontWeight: 'bold',
+                }}
+              >
+                <Box key={option.id} sx={{ mr: 1, mt: '5px' }}>
+                  <Iconify icon="humbleicons:user-add" />
+                </Box>
+                {option.name}
+              </li>
+            );
+          }
           return (
             <li {...props} key={option.id}>
               {option.name}
