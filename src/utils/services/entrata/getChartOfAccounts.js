a05ec010248ca9336d5d5483505cc82a778a955f
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-04 13:01:23 -0500

    Update Entrata Fetch Calls

--- src/utils/services/entrata/getChartOfAccounts.js
+++ src/utils/services/entrata/getChartOfAccounts.js
@@ -1,33 +1,59 @@
-import fetchData from "./fetch";
-import { chartOfAccounts } from "src/assets/data/chart-of-accounts";
-
-function condenseGlAccountData(glAccountObjects) {
-    
-    const accountToCategoryLookup = Object.entries(chartOfAccounts).reduce((lookup, [category, accountNumbers]) => {
-        accountNumbers.forEach(accountNumber => {
-            lookup[accountNumber] = category;
-        });
-        return lookup;
-    }, {});
-
-    const refinedAccounts = glAccountObjects
-        .filter(account => accountToCategoryLookup[account.AccountNumber])
-        .map(account => ({
-            accountName: account.AccountName,
-            accountId: account["@attributes"].Id,
-            accountNumber: account.AccountNumber,
-            category: accountToCategoryLookup[account.AccountNumber]
-        }));
-
-    return refinedAccounts;
-}
-
+import { ENTRATA_API } from 'src/config-global';
+import { authorization } from './authorization';
+import { chartOfAccounts } from 'src/assets/data/chart-of-accounts';
 
 export default async function getEntrataChartOfAccounts() {
-    const data = await fetchData('/api/v1/financial', "getGlTrees", 'force-cache');
-    const glAccountObjects = data.response.result.GlTrees.GlTree[0].GlAccounts.GlAccount;
-    return condenseGlAccountData(glAccountObjects);
+  const data = await fetchData();
+  const glAccountObjects = data.response.result.GlTrees.GlTree[0].GlAccounts.GlAccount;
+  return condenseGlAccountData(glAccountObjects);
 }
 
-    
-      
+async function fetchData() {
+  const endpoint = '/api/v1/financial';
+  const bodyMethod = 'getGlTrees';
+  const url = `${ENTRATA_API.baseUrl}${endpoint}`;
+  const body = {
+    auth: { type: 'basic' },
+    requestId: '15',
+    method: { name: bodyMethod },
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: authorization,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Fetch ${bodyMethod} failed:`, error);
+    throw error;
+  }
+}
+
+function condenseGlAccountData(glAccountObjects) {
+  const accountToCategoryLookup = Object.entries(chartOfAccounts).reduce((lookup, [category, accountNumbers]) => {
+    accountNumbers.forEach((accountNumber) => {
+      lookup[accountNumber] = category;
+    });
+    return lookup;
+  }, {});
+
+  const refinedAccounts = glAccountObjects
+    .filter((account) => accountToCategoryLookup[account.AccountNumber])
+    .map((account) => ({
+      accountName: account.AccountName,
+      accountId: account['@attributes'].Id,
+      accountNumber: account.AccountNumber,
+      category: accountToCategoryLookup[account.AccountNumber],
+    }));
+
+  return refinedAccounts;
+}
--- src/utils/services/entrata/getVendorLocations.js
+++ src/utils/services/entrata/getVendorLocations.js
@@ -1,35 +1,65 @@
-import fetchData from "./fetch";
+import { ENTRATA_API } from 'src/config-global';
+import { authorization } from './authorization';
+export default async function getVendorLocations() {
+  const data = await fetchData();
+  const vendorObjects = data.response.result.Locations.Location;
+  return condenseVendorData(vendorObjects);
+}
+
+async function fetchData() {
+  const endpoint = '/api/v1/vendors';
+  const bodyMethod = 'getVendorLocations';
+  const url = `${ENTRATA_API.baseUrl}${endpoint}`;
+  const body = {
+    auth: { type: 'basic' },
+    requestId: '15',
+    method: { name: bodyMethod },
+  };
+
+  try {
+    const response = await fetch(url, {
+      next: { tags: ['vendors'] },
+      method: 'POST',
+      headers: {
+        Authorization: authorization,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(`Fetch ${bodyMethod} failed:`, error);
+    throw error;
+  }
+}
 
 function condenseVendorData(vendorsObject) {
-    const vendorsArray = Object.values(vendorsObject);
-    const vendorIdMap = new Map();
-    const nameCount = {};
+  const vendorsArray = Object.values(vendorsObject);
+  const vendorIdMap = new Map();
+  const nameCount = {};
 
-    vendorsArray.forEach(vendor => {
-        const vendorId = vendor.VendorId;
-        vendorIdMap.set(vendorId, (vendorIdMap.get(vendorId) || []).concat(vendor));
-        nameCount[vendor.VendorName] = (nameCount[vendor.VendorName] || 0) + 1;
-    });
+  vendorsArray.forEach((vendor) => {
+    const vendorId = vendor.VendorId;
+    vendorIdMap.set(vendorId, (vendorIdMap.get(vendorId) || []).concat(vendor));
+    nameCount[vendor.VendorName] = (nameCount[vendor.VendorName] || 0) + 1;
+  });
 
-    return vendorsArray.map(vendor => {
-        const condensedVendor = {
-            name: vendor.VendorName,
-            id: vendor['@attributes'].Id,
-            vendorId: vendor.VendorId,
-            defaultGl: vendor.Accounts?.Account?.[0]?.GlAccountId || null
-        };
+  return vendorsArray.map((vendor) => {
+    const condensedVendor = {
+      name: vendor.VendorName,
+      id: vendor['@attributes'].Id,
+      vendorId: vendor.VendorId,
+      defaultGl: vendor.Accounts?.Account?.[0]?.GlAccountId || null,
+    };
 
-        if (nameCount[vendor.VendorName] > 1) {
-            condensedVendor.name += ` (${condensedVendor.id})`;
-        }
+    if (nameCount[vendor.VendorName] > 1) {
+      condensedVendor.name += ` (${condensedVendor.id})`;
+    }
 
-        return condensedVendor;
-    });
+    return condensedVendor;
+  });
 }
-
-export default async function getVendorLocations() {
-    const data = await fetchData('/api/v1/vendors', "getVendorLocations");
-    const vendorObjects = data.response.result.Locations.Location;
-    return condenseVendorData(vendorObjects);
-}
-
