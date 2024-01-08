7cae1f0d9b1e3a37edd4b6d6a73eb0378a590a66
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-07 21:10:00 -0500

    REFACTOR RHF - Adjust Asset Dropdown

--- src/app/dashboard/credit-cards/reports/components/DropDownAssets.js
+++ src/app/dashboard/credit-cards/reports/components/DropDownAssets.js
@@ -1,22 +1,44 @@
 'use client';
 
 import { assetItems } from 'src/assets/data/assets';
-import AutocompleteGroup from 'src/components/form-inputs/AutocompleteGroup';
+
+import Popper from '@mui/material/Popper';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import ListSubheader from '@mui/material/ListSubheader';
 
 export default function DropDownAssets({ asset, setAsset }) {
-
-  const handleAssetChange = (newValue) => {
-    setAsset(newValue);
-  };
-  
+  const currentValue = asset || null;
   return (
-    <AutocompleteGroup 
-    value={asset}
-    options={assetItems}
-    handleChange={handleAssetChange}
-    label="Location"
-    id="grouped-asset-accounts"
-    sx={{ width: '200px'}}
-    /> 
+    <Autocomplete
+      value={currentValue}
+      sx={{ width: 200 }}
+      PopperComponent={({ style, ...props }) => <Popper {...props} sx={{ ...style, height: 0 }} />}
+      defaultValue={null}
+      onChange={(event, newValue) => {
+        setAsset(newValue);
+      }}
+      id="grouped-assets"
+      options={assetItems.sort((a, b) => b.category.localeCompare(a.category))}
+      groupBy={(option) => option.category}
+      getOptionLabel={(option) => option.label}
+      renderInput={(params) => <TextField {...params} label='Location' />}
+      isOptionEqualToValue={(option, selected) => option.id === selected.id}
+      renderOption={(props, option) => (
+        <li {...props} key={option.id}>
+          {option.label}
+        </li>
+      )}
+      renderGroup={(params) => (
+        <div key={params.key}>
+          <ListSubheader
+            sx={{ fontWeight: 'bold', color: (theme) => (theme.palette.mode === 'light' ? 'primary.darker' : theme.palette.grey[500]) }}
+          >
+            {params.group}
+          </ListSubheader>
+          {params.children}
+        </div>
+      )}
+    />
   );
 }
