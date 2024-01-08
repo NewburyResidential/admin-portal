f4caf20a10520dea0636a373d0b55af4878f9d64
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-07 21:06:32 -0500

    REFACTOR RHF - Create New Inputs

--- src/app/dashboard/credit-cards/components/addVendor/Inputs.js
+++ src/app/dashboard/credit-cards/components/addVendor/Inputs.js
@@ -5,55 +5,53 @@ import { useFormStatus } from 'react-dom';
 
 export default function Inputs() {
   const { pending } = useFormStatus();
-  const methods = useFormContext();
-  const {
-    register,
-    control,
-    formState: { errors },
-  } = methods;
+  const { control } = useFormContext();
 
   return (
-    <>
-      <Stack spacing={2} mb={2} mt={2}>
-        {pending ? (
-          <>
-            <Skeleton variant="rounded" height={60} />
-            <Skeleton variant="rounded" height={60} />
-          </>
-        ) : (
-          <>
-            <TextField
-              {...register('vendor')}
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Vendor Name"
-              type="text"
-              fullWidth
-              variant="outlined"
-              error={errors?.vendor ? true : false}
-              helperText={errors?.vendor && errors.vendor.message}
-            />
-
-            <Controller
-              name="entityType"
-              control={control}
-              render={({ field }) => (
-                <FormControl fullWidth>
-                  <InputLabel id="entity-type-label">Entity Type</InputLabel>
-                  <Select {...field} labelId="entity-type-label" id="entity-type-select" label="Entity Type">
-                    {Object.entries(entityOptions).map(([key, value]) => (
-                      <MenuItem key={key} value={key}>
-                        {value}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              )}
-            />
-          </>
-        )}
-      </Stack>
-    </>
+    <Stack spacing={2} mb={2} mt={2}>
+      {pending ? (
+        <>
+          <Skeleton variant="rounded" height={60} />
+          <Skeleton variant="rounded" height={60} />
+        </>
+      ) : (
+        <>
+          <Controller
+            name="vendor"
+            control={control}
+            render={({ field, fieldState: { error } }) => (
+              <TextField
+                {...field}
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Vendor Name"
+                type="text"
+                fullWidth
+                variant="outlined"
+                error={!!error}
+                helperText={error ? error.message : null}
+              />
+            )}
+          />
+          <Controller
+            name="entityType"
+            control={control}
+            render={({ field }) => (
+              <FormControl fullWidth>
+                <InputLabel id="entity-type-label">Entity Type</InputLabel>
+                <Select {...field} labelId="entity-type-label" id="entity-type-select" label="Entity Type">
+                  {Object.entries(entityOptions).map(([key, value]) => (
+                    <MenuItem key={key} value={key}>
+                      {value}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            )}
+          />
+        </>
+      )}
+    </Stack>
   );
 }
