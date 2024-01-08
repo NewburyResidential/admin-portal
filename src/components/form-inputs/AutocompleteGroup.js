16c8d0376902700a82a6cd9f80865c17f1de79b9
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-07 21:07:41 -0500

    REFACTOR RHF - Adjust Autocomplete Shared

--- src/components/form-inputs/AutocompleteGroup.js
+++ src/components/form-inputs/AutocompleteGroup.js
@@ -1,52 +1,58 @@
+import React from 'react';
 import Popper from '@mui/material/Popper';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import ListSubheader from '@mui/material/ListSubheader';
+import { useFormContext, Controller } from 'react-hook-form';
 
 export default function AutocompleteGroup({
-  value,
+  fieldName,
   options,
-  handleChange,
+  handleChange = () => {},
   label = 'Select',
-  error = false,
   id = 'grouped-auto-complete',
   optionLabel = 'label',
   optionId = 'id',
   sx,
 }) {
-  const currentValue = value || null;
+  const { control } = useFormContext();
 
   return (
-    <Autocomplete
-      sx={sx}
-      PopperComponent={({ style, ...props }) => <Popper {...props} sx={{ ...style, height: 0 }} />}
-      value={currentValue}
-      defaultValue={null}
-      onChange={(event, newValue) => {
-        handleChange(newValue);
-      }}
-      id={id}
-      options={options.sort((a, b) => b.category.localeCompare(a.category))}
-      groupBy={(option) => option.category}
-      getOptionLabel={(option) => option[optionLabel]}
-      renderInput={(params) => <TextField {...params} label={label} error={error} />}
-      isOptionEqualToValue={(option, selected) => option[optionId] === selected[optionId]}
-      renderOption={(props, option) => {
-        return (
-          <li {...props} key={option[optionId]}>
-            {option[optionLabel]}
-          </li>
-        );
-      }}
-      renderGroup={(params) => (
-        <div key={params.key}>
-          <ListSubheader
-            sx={{ fontWeight: 'bold', color: (theme) => (theme.palette.mode === 'light' ? 'primary.darker' : theme.palette.grey[500]) }}
-          >
-            {params.group}
-          </ListSubheader>
-          {params.children}
-        </div>
+    <Controller
+      name={fieldName}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <Autocomplete
+          {...field}
+          sx={sx}
+          PopperComponent={({ style, ...props }) => <Popper {...props} sx={{ ...style, height: 0 }} />}
+          defaultValue={null}
+          onChange={(event, newValue) => {
+            field.onChange(newValue);
+            handleChange(newValue);
+          }}
+          id={id}
+          options={options.sort((a, b) => b.category.localeCompare(a.category))}
+          groupBy={(option) => option.category}
+          getOptionLabel={(option) => option[optionLabel]}
+          renderInput={(params) => <TextField {...params} label={label} error={!!error} />}
+          isOptionEqualToValue={(option, selected) => option[optionId] === selected[optionId]}
+          renderOption={(props, option) => (
+            <li {...props} key={option[optionId]}>
+              {option[optionLabel]}
+            </li>
+          )}
+          renderGroup={(params) => (
+            <div key={params.key}>
+              <ListSubheader
+                sx={{ fontWeight: 'bold', color: (theme) => (theme.palette.mode === 'light' ? 'primary.darker' : theme.palette.grey[500]) }}
+              >
+                {params.group}
+              </ListSubheader>
+              {params.children}
+            </div>
+          )}
+        />
       )}
     />
   );
