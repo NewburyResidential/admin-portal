b2235addbbb29c8a200730a4d54aa3158ebb5adc
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-07 21:04:30 -0500

    REFACTOR RHF - Rebuild Table

--- src/app/dashboard/credit-cards/components/Table.js
+++ src/app/dashboard/credit-cards/components/Table.js
@@ -1,306 +1,94 @@
 'use client';
 
-import { useState, useCallback } from 'react';
+import { useForm, useFieldArray, FormProvider, useWatch } from 'react-hook-form';
+import { transactionsSchema } from './utils/transactions-schema';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import Paper from '@mui/material/Paper';
+import CardActions from '@mui/material/CardActions';
 import TableContainer from '@mui/material/TableContainer';
-import LoadingButton from '@mui/lab/LoadingButton';
 
-import TablePagination from '@mui/material/TablePagination';
-
-import RowItem from './RowItem';
-import { isIncorrectAmounts, isMissingValue } from 'src/utils/expense-calculations/missing-value';
+import PageChange from './PageChange';
+import RowItem from './rowItems/RowItem';
+import ButtonApprove from './ButtonApprove';
 import updateTransactions from 'src/utils/services/cc-expenses/updateTransactions';
 
-import { useTheme } from '@mui/material/styles';
-import VendorDialog from './addVendor/Dialog';
-
 export default function CustomTable({ user, vendors, chartOfAccounts, unapprovedTransactions }) {
-  const theme = useTheme();
-  const isLight = theme.palette.mode === 'light';
+  const methods = useForm({
+    defaultValues: {
+      transactions: unapprovedTransactions,
+      pageSettings: { page: 0, rowsPerPage: 10 },
+    },
+    resolver: yupResolver(transactionsSchema),
+  });
 
-  const [vendorDialog, setVendorDialog] = useState({ open: true, defaultValue: '' });
-  const [loading, setLoading] = useState(false);
+  const { control, handleSubmit } = methods;
 
-  const [transactions, setTransactions] = useState(() =>
-    unapprovedTransactions.map((transaction) => ({
-      ...transaction,
-      checked: false,
-    }))
-  );
+  const { page, rowsPerPage } = useWatch({
+    control,
+    name: `pageSettings`,
+  });
 
-  const haveSelected = transactions.some((transaction) => transaction.checked);
-  const selectedTransactions = transactions.reduce((count, transaction) => {
-    return transaction.checked ? count + 1 : count;
-  }, 0);
+  const { fields: transactionFields, remove } = useFieldArray({
+    control,
+    name: `transactions`,
+  });
+  const currentPageTransactions = transactionFields.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage);
 
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const [page, setPage] = useState(0);
+  const onSubmit = async (data) => {
+    const transactionIndexData = data.transactions
+      .map((transaction, index) => (transaction.checked ? { index, id: transaction.id } : null))
+      .filter((item) => item !== null);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
+    const validTransactions = data.transactions
+      .filter((transaction) => transaction.checked)
+      .map((transaction) => ({
+        ...transaction,
+        status: 'unapproved',
+        approvedBy: user.name,
+      }));
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleCheckboxToggle = useCallback((transactionId) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId ? { ...transaction, checked: !transaction.checked } : transaction
-      )
-    );
-  }, []);
-
-  const handleAllocationAmountChange = useCallback((transactionId, allocationId, newAmount) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              allocations: transaction.allocations.map((allocation) =>
-                allocation.id === allocationId ? { ...allocation, amount: newAmount } : allocation
-              ),
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleAddSplit = useCallback((transactionId, newAllocationId) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              allocations: [
-                ...transaction.allocations.map((allocation) => ({
-                  ...allocation,
-                  amount: 0,
-                })),
-                { id: newAllocationId, amount: 0, glAccount: null, note: '', vendor: null, assets: [] },
-              ],
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleDeleteSplit = useCallback((transactionId, allocationId) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) => {
-        if (transaction.id === transactionId) {
-          const newAllocations = transaction.allocations.filter((allocation) => allocation.id !== allocationId);
-
-          if (newAllocations.length === 1) {
-            newAllocations[0].amount = transaction.amount;
-          } else {
-            newAllocations.forEach((allocation) => {
-              allocation.amount = 0;
-            });
-          }
-
-          return {
-            ...transaction,
-            allocations: newAllocations,
-          };
-        }
-        return transaction;
-      })
-    );
-  }, []);
-
-  const handleGlAccountChange = useCallback((transactionId, allocationId, newGlAccount) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              allocations: transaction.allocations.map((allocation) =>
-                allocation.id === allocationId ? { ...allocation, glAccount: newGlAccount } : allocation
-              ),
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleAssetsChange = useCallback((transactionId, allocationId, newAsset) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              allocations: transaction.allocations.map((allocation) =>
-                allocation.id === allocationId ? { ...allocation, asset: newAsset } : allocation
-              ),
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleVendorChange = useCallback((transactionId, newVendor) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              vendor: newVendor,
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleNoteChange = useCallback((transactionId, allocationId, newNote) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              allocations: transaction.allocations.map((allocation) =>
-                allocation.id === allocationId ? { ...allocation, note: newNote } : allocation
-              ),
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  const handleReceiptChange = useCallback((transactionId, fileUrl, tempPdfUrl) => {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) =>
-        transaction.id === transactionId
-          ? {
-              ...transaction,
-              receipt: fileUrl,
-              tempPdfReceipt: tempPdfUrl,
-            }
-          : transaction
-      )
-    );
-  }, []);
-
-  function setTransactionSubmitted(transactionId) {
-    setTransactions((prevTransactions) =>
-      prevTransactions.map((transaction) => (transaction.id === transactionId ? { ...transaction, isSubmitted: true } : transaction))
-    );
-  }
-
-  const handleApproveTransactions = async () => {
-    const validTransactions = [];
-    transactions.forEach((transaction) => {
-      if (transaction.checked) {
-        let transactionValid = true;
-        const errors = [];
-        setTransactionSubmitted(transaction.id);
-        const isVendorRequired = transaction.allocations.some(
-          (allocation) => allocation.asset && allocation.asset.accountingSoftware === 'entrata'
-        );
-
-        if (isVendorRequired && isMissingValue(transaction.vendor)) {
-          transactionValid = false;
-          errors.push(`Item ID: ${transaction.id} is missing vendor`);
-        }
-
-        if (isIncorrectAmounts(transaction)) {
-          transactionValid = false;
-          errors.push(`Item ID: ${transaction.id} has incorrect allocations`);
-        }
-
-        transaction.allocations.forEach((allocation) => {
-          const missingFields = [];
-          if (isMissingValue(allocation.asset)) missingFields.push('assets');
-          if (isMissingValue(allocation.glAccount)) missingFields.push('glAccount');
-          if (missingFields.length > 0) {
-            errors.push(`Allocation ID ${allocation.id} is missing: ${missingFields.join(', ')}`);
-            transactionValid = false;
-          }
-        });
-        if (transactionValid) {
-          validTransactions.push({ ...transaction, status: 'reviewed', approvedBy: user.name });
-        } else {
-          console.error(errors.join('\n'));
-        }
+    const updateTransactionPromises = validTransactions.map((transaction) => updateTransactions([transaction]));
+    const responses = await Promise.all(updateTransactionPromises);
+    const updatedTransactionIds = responses.reduce((acc, response) => {
+      if (response && response.ids) {
+        acc.push(...response.ids);
       }
-    });
-    if (validTransactions.length > 0) {
-      setLoading(true);
-      try {
-        const response = await updateTransactions(validTransactions);
-        if (response.ids.length > 0) {
-          const updatedTransactionIds = response.ids;
-          setTransactions((prevTransactions) => prevTransactions.filter((transaction) => !updatedTransactionIds.includes(transaction.id)));
-        }
-      } catch (error) {
-        setLoading(false);
-        console.error('Error Updating Transactions: ', error);
-      }
-    }
-    setLoading(false);
+      return acc;
+    }, []);
+
+    const indicesToRemove = transactionIndexData.filter((item) => updatedTransactionIds.includes(item.id)).map((item) => item.index);
+    remove(indicesToRemove);
   };
+
   return (
-    <>
-      <VendorDialog
-        open={vendorDialog.open}
-        defaultValue={vendorDialog?.defaultValue}
-        handleClose={() => setVendorDialog({ open: false, defaultValue: '' })}
-      />
-      <Card sx={{ borderRadius: '10px' }}>
-        <CardActions sx={{ backgroundColor: isLight ? 'primary.darker' : theme.palette.common.black }}>
-          <LoadingButton
-            variant="contained"
-            style={{ marginLeft: '16px', width: '140px' }}
-            disabled={!haveSelected}
-            type="submit"
-            color="primary"
-            loading={loading}
-            onClick={handleApproveTransactions}
+    <FormProvider {...methods}>
+      <form action={handleSubmit(onSubmit)}>
+        <Card sx={{ borderRadius: '10px' }}>
+          <CardActions
+            sx={{ backgroundColor: (theme) => (theme.palette.mode === 'light' ? 'primary.darker' : theme.palette.common.black) }}
           >
-            Approve {selectedTransactions > 0 && `(${selectedTransactions})`}
-          </LoadingButton>{' '}
-          <TablePagination
-            sx={{ color: 'white' }}
-            rowsPerPageOptions={[10, 25, 50]}
-            component="div"
-            count={transactions.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </CardActions>
-        <TableContainer component={Paper} sx={{ borderRadius: '0px', overflowX: 'hidden' }}>
-          <Box sx={{ height: '100%', overflowX: 'hidden', display: 'flex', flexDirection: 'column' }}>
-            {transactions.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((item, index) => (
-              <Box key={item.id} sx={{ display: 'flex', flexDirection: 'column' }}>
-                <RowItem
-                  item={item}
-                  index={index}
-                  vendors={vendors}
-                  chartOfAccounts={chartOfAccounts}
-                  handleAllocationAmountChange={handleAllocationAmountChange}
-                  handleAddSplit={handleAddSplit}
-                  handleDeleteSplit={handleDeleteSplit}
-                  handleGlAccountChange={handleGlAccountChange}
-                  handleReceiptChange={handleReceiptChange}
-                  handleNoteChange={handleNoteChange}
-                  handleVendorChange={handleVendorChange}
-                  handleAssetsChange={handleAssetsChange}
-                  handleCheckboxToggle={handleCheckboxToggle}
-                  handleOpenVendorDialog={(newValue) => setVendorDialog({ open: true, defaultValue: newValue })}
-                />
-              </Box>
-            ))}
-          </Box>
-        </TableContainer>
-      </Card>
-    </>
+            <ButtonApprove transactions={transactionFields} />
+            <PageChange transactions={transactionFields} page={page} rowsPerPage={rowsPerPage} />
+          </CardActions>
+          <TableContainer component={Paper} sx={{ borderRadius: '0px', overflowX: 'hidden' }}>
+            <Box sx={{ height: '100%', overflowX: 'hidden', display: 'flex', flexDirection: 'column' }}>
+              {currentPageTransactions.map((transaction, transactionIndex) => (
+                <Box key={transaction.id} sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <RowItem
+                    transaction={transaction}
+                    transactionIndex={page * rowsPerPage + transactionIndex}
+                    vendors={vendors}
+                    chartOfAccounts={chartOfAccounts}
+                  />
+                </Box>
+              ))}
+            </Box>
+          </TableContainer>
+        </Card>
+      </form>
+    </FormProvider>
   );
 }
