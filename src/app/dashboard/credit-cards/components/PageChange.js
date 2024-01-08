3b2d2f46607666f1b8e6b381bc7bd9f7e1f7d9dc
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-01-07 21:05:01 -0500

    REFACTOR RHF - Create Page change Component

---
+++ src/app/dashboard/credit-cards/components/PageChange.js
@@ -0,0 +1,28 @@
+import { useFormContext } from 'react-hook-form';
+import TablePagination from '@mui/material/TablePagination';
+
+export default function PageChange({ transactions, page, rowsPerPage }) {
+  const { setValue } = useFormContext();
+
+  const handleChangePage = (event, newPage) => {
+    setValue('pageSettings.page', newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setValue('pageSettings.rowsPerPage', parseInt(event.target.value, 10));
+    setValue('pageSettings.page', 0);
+  };
+
+  return (
+    <TablePagination
+      sx={{ color: 'white' }}
+      rowsPerPageOptions={[10, 25, 50]}
+      component="div"
+      count={transactions.length}
+      rowsPerPage={rowsPerPage}
+      page={page}
+      onPageChange={handleChangePage}
+      onRowsPerPageChange={handleChangeRowsPerPage}
+    />
+  );
+}
