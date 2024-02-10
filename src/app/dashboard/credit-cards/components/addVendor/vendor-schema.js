a9caaf3737a1bae7cd4d3357835e73bf9d9535c1
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-02-10 14:55:48 -0500

    Vendor Schema

--- src/app/dashboard/credit-cards/components/addVendor/vendor-schema.js
+++ src/app/dashboard/credit-cards/components/addVendor/vendor-schema.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const vendorSchema = yup.object().shape({
   vendor: yup
     .string()
-    .matches(/^[A-Za-z0-9]*$/, 'Vendor can only contain letters or numbers')
+    .matches(/^[A-Za-z0-9 ]*$/, 'Vendor can only contain letters, numbers, or spaces')
     .required('Vendor name is required'),
   entityType: yup.string().required('Entity type is required'),
   informationReturn: yup.boolean(),
