b7a36a79c32d971ba855b39f12da33a73c02a6dc
Michael Axiotakis <maxiotakis@newburyresidential.com>
2024-06-08 23:55:45 -0400

    video

--- src/app/onboarding/[employee]/components/WelcomeVideo.js
+++ src/app/onboarding/[employee]/components/WelcomeVideo.js
@@ -3,10 +3,10 @@ import Box from '@mui/material/Box';
 
 export default function WelcomeVideo() {
   return (
-    <Card sx={{ maxWidth: '100%', height: '500px', mt: 4, overflow: 'hidden' }}>
+    <Card sx={{ maxWidth: '100%', height: '600px', mt: 4, overflow: 'hidden' }}>
       <Box sx={{ width: '100%', height: '100%' }}>
         <video
-          src="https://newbury-intranet.s3.amazonaws.com/alabama-covervideo.mp4"
+          src="https://newbury-intranet.s3.amazonaws.com/video-demo.mp4"
           title="Welcome Video"
           controls
           style={{ width: '100%', height: '100%', objectFit: 'cover' }}
