ab6d344eff90387929a7703ff0849f79d5055be2
MikeAxio <MikeAxiotakis@gmail.com>
2025-07-18 13:14:09 -0400

    Cache Chart of Accounts

--- src/utils/services/utility/getAllChartOfAccounts.js
+++ src/utils/services/utility/getAllChartOfAccounts.js
@@ -1,19 +1,20 @@
-import getEntrataChartOfAccounts from "../entrata/getChartOfAccounts";
-import getWaveChartOfAccounts from "../wave/getWaveChartOfAccounts";
+import getEntrataChartOfAccounts from '../entrata/getChartOfAccounts';
+import getWaveChartOfAccounts from '../wave/getWaveChartOfAccounts';
+import { unstable_cache } from 'next/cache';
 
-export default async function getAllChartOfAccounts() {
-    const [entrataChartOfAccounts, waveChartOfAccounts] = await Promise.all([
-        getEntrataChartOfAccounts(),
-        getWaveChartOfAccounts(),
-    ])
+async function getAllChartOfAccountsRaw() {
+  console.log('Missed cache: fetching all chart of accounts');
+  const [entrataChartOfAccounts, waveChartOfAccounts] = await Promise.all([getEntrataChartOfAccounts(), getWaveChartOfAccounts()]);
 
-    entrataChartOfAccounts.forEach(entrataAccount => {
-        const waveAccount = waveChartOfAccounts.find(account => account.displayId === entrataAccount.accountNumber);
-        if (waveAccount) {
-            entrataAccount.waveGlArray = waveAccount.accounts;
-        }
-    });
+  entrataChartOfAccounts.forEach((entrataAccount) => {
+    const waveAccount = waveChartOfAccounts.find((account) => account.displayId === entrataAccount.accountNumber);
+    if (waveAccount) {
+      entrataAccount.waveGlArray = waveAccount.accounts;
+    }
+  });
 
-    return entrataChartOfAccounts;
-
+  return entrataChartOfAccounts;
 }
+
+
+export default unstable_cache(getAllChartOfAccountsRaw, ['get-all-chart-of-accounts'], { tags: ['chart-of-accounts'] });
